6b057ce7d36379969bf886ecc2bb312773368cd7
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-05 23:10:54 +0300

    create route to login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Routes, Route, useRoutes } from "react-router-dom";
+import Login from "./component/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
