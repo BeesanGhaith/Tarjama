bb67bd4d50d76b32101c0bd44ae9a64c1fe8cd28
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-05 23:06:56 +0300

    Create Reducer

---
+++ src/reducer/login/index.js
@@ -0,0 +1,33 @@
+const initialState = {
+    user: "" || localStorage.getItem("user"),
+    isLoggedIn: localStorage.getItem("user") ? true : false,
+  };
+  
+  const loginReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+      case "LOG_IN":
+        return {
+          user: payload,
+          isLoggedIn: true,
+        };
+      case "LOG_OUT":
+        return {
+          user: "",
+          isLoggedIn: false,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default loginReducer;
+  
+  export const login = (user) => {
+    return { type: "LOG_IN", payload: user };
+  };
+  
+  export const logout = () => {
+    return { type: "LOG_OUT" };
+  };
+  
