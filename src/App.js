70db10a53a244c1262a5a5b8f6bc4688edd63a35
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-06 01:27:39 +0300

    Create route to home page in app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import React, { useState } from "react";
 import { Routes, Route, useRoutes } from "react-router-dom";
 import Login from "./component/Login/Login";
+import Home from "./component/Home/Home";
+import Navigation from "./component/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
