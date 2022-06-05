ab3f6259872ab2d3eba7091d4d63031356ec6c92
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-05 23:10:06 +0300

    use Provider and BrowserRouter in App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// Import Provider
+import { Provider } from "react-redux";
+//Import Store
+import store from "./reducer/index";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
