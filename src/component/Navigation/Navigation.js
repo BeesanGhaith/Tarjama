257ec814d35281d40a932c1065b43aaf5d084f20
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-07 19:37:57 +0300

    add icons in navbar

--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -3,6 +3,10 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../reducer/login/index";
 import { useNavigate } from "react-router-dom";
+import { BiUserPin } from "react-icons/bi";
+import { MdLogout } from "react-icons/md";
+import { GoHome } from "react-icons/go";
+
 
 function Navigation() {
   const dispatch = useDispatch();
@@ -17,25 +21,40 @@ function Navigation() {
   const navigate = useNavigate();
 
   const logoutUser = () => {
-      dispatch(logout());
-      localStorage.clear();
-      navigate("/");
-  }
-
-
+    dispatch(logout());
+    localStorage.clear();
+    navigate("/");
+  };
 
   return (
     <>
       {state.isLoggedIn ? (
         <>
           <div className="div-navbar">
-            <h2 className="name" onClick={() => {
-              navigate("/profile")
-            }}>{state.user.name}</h2>
-            <h2 onClick={() => {
-              navigate("/home")
-            }}>Home</h2>
-            <h2 className="logout" onClick={logoutUser}>LogOut</h2>
+            <div><GoHome className="home-icon"/>
+            <h2
+              className="home"
+              onClick={() => {
+                navigate("/home");
+              }}
+            >
+              Home
+            </h2></div>
+            <div>
+                <BiUserPin className="user-icon"/>
+                <h2
+                  className="name"
+                  onClick={() => {
+                    navigate("/profile");
+                  }}
+                >
+                  {state.user.name}
+                </h2>
+                <MdLogout className="logout-icon"/>
+              <h2 className="logout" onClick={logoutUser}>
+                LogOut
+              </h2>
+            </div>
           </div>
         </>
       ) : (
