49e8d4c4ac92b78c194a9dae97038deeb8de988c
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-07 03:10:34 +0300

    import posts reducer and users reducer in store

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,8 +3,10 @@ import { createStore, combineReducers } from "redux";
 
 // Import Reducers
 import loginReducer from "./login/index";
+import postsReducer from "./posts/index";
+import usersReducer from "./users/index";
 
-const reducers = combineReducers({ loginReducer });
+const reducers = combineReducers({ loginReducer, postsReducer, usersReducer });
 const store = createStore(reducers);
 
 export default store;
