43eae45d8da9ede51148cfc52fbafbf287151de9
BeesanGhaith <beesan.ghaith@gmail.com>
2022-06-07 03:04:10 +0300

    modify App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import { Routes, Route, useRoutes } from "react-router-dom";
 import Login from "./component/Login/Login";
 import Home from "./component/Home/Home";
 import Navigation from "./component/Navigation/Navigation";
+import Profile from "./component/Profile/Profile";
+import Users from "./component/Users/Users";
 
 function App() {
+  const [profile, setProfile] = useState();
   return (
     <div className="App">
-      <Navigation />
+      <Navigation profile={profile}/>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </div>
   );
